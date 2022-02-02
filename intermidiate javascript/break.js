999ff02fdc98c6335967cde10ec1b54f9e6b165c
Md Mehedi Hasan <45948603+mehedi-01@users.noreply.github.com>
2022-02-02 13:53:41 +0600

    added

--- intermidiate javascript/break.js
+++ intermidiate javascript/break.js
@@ -1,6 +1,6 @@
 const nums = [1, 2, 3, 4, -10, -13, 5, 7, 8, 9];
 for (let i = 0; i < nums.length; i++) {
-    if (nums[i] <8) {
+    if (nums[i] <1 ) {
         break;
     }
     console.log(nums[i]);
